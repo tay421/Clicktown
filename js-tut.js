1fee11f6ad7cabc802813ac2d034a3879e3cc7bf
tay421 <tay421isawesome@gmail.com>
2018-07-02 22:27:00 -0400

    added css tutorial file and js tutorial files and edited html to comply with new files

---
+++ js-tut.js
@@ -0,0 +1,21 @@
+var increment = 1;
+var roundExample = 0.51;
+
+if (increment != null){
+    increment = increment + 1;
+    document.getElementById("DPS").innerHTML = "DPS: " + increment;
+}
+
+//***ROUNDING EXAMPLE
+//document.write(roundExample.toFixed(1));
+
+//HTML Break statement: <br />
+
+//***RANDOM EXAMPLE
+//var rand = Math.random() * 10 + 1;
+
+//***STRING TO NUM
+//Number("String");
+
+//***STRING LENGTH
+//randStr.length
